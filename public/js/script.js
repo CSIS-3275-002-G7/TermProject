899ef5c7149e36a7570a9ff0ba4efe89237d5b7c
volkov <volkov0kostya@gmail.com>
2021-03-30 22:01:25 -0700

    ui webpage done

---
+++ public/js/script.js
@@ -0,0 +1,97 @@
+
+window.onload = function () {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var mmPlusOne = today.getMonth() + 2;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    if (mmPlusOne < 10) {
+        mmPlusOne = '0' + mmPlusOne
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+    plusMonth = yyyy + '-' + mmPlusOne + '-' + dd;
+
+    document.getElementById("appDate").setAttribute("min", today);
+    document.getElementById("appDate").setAttribute("max", plusMonth);
+
+
+};
+document.getElementById("docSelect").selectedIndex = -1;
+
+
+const validate = (form) => {
+    validateEmail(form);
+    validateName(form);
+    validateDate(form);
+    validateDoc(form);
+
+
+};
+
+validateEmail = (form) => {
+    const email = form.elements["email"];
+
+    if (email.validity.valueMissing) {
+        email.setCustomValidity("Please enter your email")
+    } else if (email.validity.typeMismatch) {
+        const input = document.getElementById("inputEmail").value;
+        email.setCustomValidity("Email " + input + " is not in a correct format");
+    } else {
+        email.setCustomValidity("");
+    }
+};
+
+
+validateName = (form) => {
+    const nameField = form.elements["name"];
+    const name = document.getElementById('inputName').value;
+    const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+
+    if (nameField.validity.valueMissing) {
+        nameField.setCustomValidity("Please enter your full legal name");
+    } else if (!regName.test(name)) {
+        nameField.setCustomValidity("Please enter your full legal First and Last name");
+    }
+    else {
+        nameField.setCustomValidity("");
+    }
+};
+
+
+
+validateDate = (form) => {
+    const date = form.elements["date"];
+    if (date.validity.valueMissing) {
+        date.setCustomValidity("Please select appointment date")
+    } else {
+        date.setCustomValidity("");
+    }
+};
+
+function docEnable() {
+    document.getElementById("docSelect").disabled = false;
+}
+
+
+validateDoc = (form) => {
+    const doc = form.elements["docSelect"];
+    if (document.getElementById("docSelect").selectedIndex < 0) {
+        doc.setCustomValidity("Please choose your doctor");
+    }
+    else {
+        doc.setCustomValidity("");
+    }
+};
+
+
+function showAppointments() {
+    document.getElementById("bookSpotContainer").className = "d-block";
+}
+
---
+++ src/Appointments.js
@@ -0,0 +1,42 @@
+export const appointments = [
+    {
+        id: 1,
+        time: '8am'
+    },
+    {
+        id: 2,
+        time: '9am'
+    },
+    {
+        id: 3,
+        time: '10am'
+    },
+    {
+        id: 4,
+        time: '11am'
+    },
+    {
+        id: 5,
+        time: '12pm'
+    },
+    {
+        id: 6,
+        time: '1pm'
+    },
+    {
+        id: 7,
+        time: '2pm'
+    },
+    {
+        id: 8,
+        time: '3pm'
+    },
+    {
+        id: 9,
+        time: '4pm'
+    },
+    {
+        id: 10,
+        time: '5pm'
+    },
+];
