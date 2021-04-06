a2b236549d803ad39152cc5807db7050040ab0c0
Michael Levitski <=>
2021-04-05 22:35:27 -0700

    add date and available properties

--- src/main/java/com/example/clinic/appointments/Appointment.java
+++ src/main/java/com/example/clinic/appointments/Appointment.java
@@ -1,19 +1,40 @@
 package com.example.clinic.appointments;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "appointments")
 public class Appointment {
+
+    @Id
     private Integer appointmentId;
     private String name;
+    private String date;
     private String time;
+    private boolean available;
 
-    public int getAppointmentId() {
-        return appointmentId;
+    public boolean isAvailable() {
+        return available;
     }
 
-    public void setAppointmentId(int appointmentId) {
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void setAppointmentId(Integer appointmentId) {
         this.appointmentId = appointmentId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Integer getAppointmentId() {
+        return appointmentId;
     }
 
     public String getName() {
